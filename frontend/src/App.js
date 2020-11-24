5357de4c579afe7328493516925d1b8363831c69
Simrandeep Singh <61223369+simranmukerian@users.noreply.github.com>
2020-11-24 11:13:39 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import "./App.css";
 import Main from "./components/Main";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
 
 //App Component
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        {/*Use Browser Router to route to different pages*/}
+        /*Use Browser Router to route to different pages*/
         <BrowserRouter>
           <div>
             {/* App Component Has a Child Component called Main*/}
             <Main />
+            <Switch>
+             <Route path="/Home" component={Home} exact/>
+             <Route path="/Login" component={Login}/>
+             <Route path="/Signup" component={Signup}/>
+           </Switch>
           </div>
         </BrowserRouter>
-      </Provider>
     );
-  }
+}
 }
 //Export the App component so that it can be used in index.js
 export default App;
