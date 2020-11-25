377b711c9cdb1498ab302c24b58dbc98014f39a5
Shravya-Uchil <shravya.uchil@sjsu.edu>
2020-11-25 14:30:36 -0800

    Database connection using AWS RDS

--- backend/mysqlDB.js
+++ backend/mysqlDB.js
@@ -1,16 +1,17 @@
-var mysql = require("mysql");
+var mysql = require('mysql');
 var connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
+  // host: "localhost",
+  host: 'database-1.crfdq7kjsupx.us-west-2.rds.amazonaws.com',
+  user: 'admin',
   port: 3306,
-  password: "Mysqlpwd123",
-  database: "farm-mgmt",
+  password: 'farmpwd123',
+  database: 'farm-mgmt',
   insecureAuth: true,
 });
 
 connection.getConnection((err) => {
   if (err) {
-    throw "Error occured: " + err;
+    throw 'Error occured: ' + err;
   }
 });
 
