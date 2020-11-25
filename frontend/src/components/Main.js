73b1691ab9752aed8f33067d9be07f3a9b87e7a2
Simrandeep Singh <61223369+simranmukerian@users.noreply.github.com>
2020-11-25 09:06:13 -0800

    Update Main.js

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -8,6 +8,8 @@ const Main = () => {
           <NavLink to="/Home">Home </NavLink>
           <NavLink to="/Login">Login </NavLink>
           <NavLink to="/Signup">Signup </NavLink>
+          <h1>Smithfield Farms</h1>
+          <p>Welcome to the Farm Management Webpage!</p>
        </div>
     );
 }
